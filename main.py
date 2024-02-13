63f93ab284c8bfbf711541012647a609226c40c6
Mihir Katyal <mkatyal@uci.edu>
2024-02-12 21:59:18 -0800

    updated def procees args

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ from key_validation import validate_key, InvalidKeyExecption
 from crypto import encrypt, decrypt
 
 def procces_arg(args):
-    if lens(args) !=5
-    print("Usage: python main.py -e/-d input_file output_file key")
-    sys.exit(1)
-    
+    if len(args) != 5:
+        print("Usage: python main.py -e/-d input_file output_file key")
+        sys.exit(1)
+
+    mode = args[1]
+    input_file = args[2]
+    output_file = args[3]
+    try:
+        key = int(args[4])
+    except ValueError:
+        print("Key must be an integer")
+        sys.exit(1)
+        
