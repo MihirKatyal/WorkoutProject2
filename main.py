b64c91ac553c461b6dea19e8d17da9d6f8545741
Mihir Katyal <mkatyal@uci.edu>
2024-02-12 21:58:05 -0800

    created def procces args

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import sys
 from key_validation import validate_key, InvalidKeyExecption
 from crypto import encrypt, decrypt
+
+def procces_arg(args):
+    if lens(args) !=5
+    print("Usage: python main.py -e/-d input_file output_file key")
+    sys.exit(1)
+    
