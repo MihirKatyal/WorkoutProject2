c1262f592cc3eef2e08fe57dce03592ebb67b708
Mihir Katyal <mkatyal@uci.edu>
2024-02-12 22:04:52 -0800

    finished main.py

--- main.py
+++ main.py
@@ -46,3 +46,6 @@ def main():
     except Exception as e:
         print(f"Error during {'encryption' if mode == '-e' else 'decryption'}: {e}")
         sys.exit(1)
+
+if __name__ == "__main__":
+    main()
